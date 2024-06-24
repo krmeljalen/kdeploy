7d0e00b792a14afd8afb44dcf09b3e82fe29c01b
Alen Krmelj <krmelj.alen@gmail.com>
2024-06-24 02:36:54 +0200

    Support for cronjob

--- kdeploy/kkubernetes.py
+++ kdeploy/kkubernetes.py
@@ -52,13 +52,54 @@ class kkubernetes:
 
         print(f"Pod {app_name} successfully deployed in namespace '{namespace}'")
 
-    def deploy_deployment(self, manifest, namespace="default"):
-        print(f"Deploying Deployment {manifest['app_name']}")
-        pass
-
     def deploy_cronjob(self, manifest, namespace="default"):
-        print(f"Deploying Cronjob {manifest['app_name']}")
-        schedule = manifest["kubernetes"]["schedule"]
+        app_name = manifest["app_name"]
+        docker_repo_label = manifest["docker_repo_label"]
+        image_tag = helper.generate_image_tag(manifest)
+        cronjob_schedule = manifest["kubernetes"]["schedule"]
+
+        print(f"Deploying Cronjob {app_name}")
+
+        cronjob_manifest = client.V1beta1CronJob(
+            api_version="batch/v1",
+            kind="CronJob",
+            metadata=client.V1ObjectMeta(name=app_name),
+            spec=client.V1beta1CronJobSpec(
+                schedule=cronjob_schedule,
+                job_template=client.V1beta1JobTemplateSpec(
+                    spec=client.V1JobSpec(
+                        template=client.V1PodTemplateSpec(
+                            spec=client.V1PodSpec(
+                                image_pull_secrets=[
+                                    client.V1LocalObjectReference(
+                                        name=docker_repo_label
+                                    )
+                                ],
+                                containers=[
+                                    client.V1Container(name=app_name, image=image_tag)
+                                ],
+                                restart_policy="Never",
+                            )
+                        )
+                    )
+                ),
+                successful_jobs_history_limit=1,
+                failed_jobs_history_limit=1,
+            ),
+        )
+
+        try:
+            self.client.create_namespaced_cron_job(
+                namespace=namespace, body=cronjob_manifest
+            )
+        except ApiException as e:
+            helper.error(f"Exception when creating CronJob: {e}")
+
+        print(f"CronJob '{app_name}' created successfully.")
+
+    def deploy_deployment(self, manifest, namespace="default"):
+        app_name = manifest["app_name"]
+        print(f"Deploying Deployment {app_name}")
         pass
 
     def deploy_service(self, manifest, namespace="default"):
@@ -71,31 +112,34 @@ class kkubernetes:
         number = 1
         for port_pair in manifest["kubernetes"]["ports"]:
             for sport, dport in port_pair.items():
-                service_ports.append(client.V1ServicePort(
+                service_ports.append(
+                    client.V1ServicePort(
                         name=f"svcport{number}",
                         port=sport,
                         protocol="TCP",
-                        target_port=dport
-                    ))
+                        target_port=dport,
+                    )
+                )
                 number += 1
 
         service_manifest = client.V1Service(
             api_version="v1",
             kind="Service",
             metadata=client.V1ObjectMeta(
-                name="obweb",
-                labels={"app.kubernetes.io/name": app_name}
+                name="obweb", labels={"app.kubernetes.io/name": app_name}
             ),
             spec=client.V1ServiceSpec(
-                selector={"app.kubernetes.io/name": app_name},
-                ports=service_ports
-            )
+                selector={"app.kubernetes.io/name": app_name}, ports=service_ports
+            ),
         )
         try:
-            self.client.create_namespaced_service(namespace=namespace, body=service_manifest)
-            print(f"Service '{app_name}' created successfully.")
+            self.client.create_namespaced_service(
+                namespace=namespace, body=service_manifest
+            )
         except ApiException as e:
-            print(f"Exception when creating Service: {e}")
+            helper.error(f"Exception when creating Service: {e}")
+
+        print(f"Service '{app_name}' created successfully.")
 
     def deploy(self, manifest):
         kind = manifest["kubernetes"]["kind"]
